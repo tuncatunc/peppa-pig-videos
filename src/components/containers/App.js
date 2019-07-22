61b3608bf1f3135f74e02e90781a450af307e53e
Tunca Tunc <atuncatunc@gmail.com>
2019-07-22 22:46:30 +0300

    Configure BrowserRouter

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -4,6 +4,8 @@ import WebPlayer from './WebPlayer'
 import GlobalStyle from '../styles/GlobalStyle'
 
 const App = props => (
+  // The video player will be deployed to root folder
+  // Otherwise set <BrowserRouter basename='subfolder name'>
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={WebPlayer}></Route>
